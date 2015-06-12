137fde91634a58d737eed3e50272295f50691ec5
edgar <ing.edgar.arguelles@gmail.com>
2015-06-12 13:41:14 -0500

    add add middleware

--- src/test/routes/module1/user.js
+++ src/test/routes/module1/user.js
@@ -2,9 +2,9 @@ var request = require('request'),
     assert = require('assert');
 
 describe('GET /', function () {
-    it("should respond with status 200", function (done) {
+    it("user UNAUTHORIZED should respond with status 401", function (done) {
         request('http://localhost:3000/api/users', function (err, resp) {
-            assert.equal(resp.statusCode, 200);
+            assert.equal(resp.statusCode, 401);
             done();
         });
     });
